c04c89d1c09c1549cc718eb7efc28a3d28053364
didierdewasseige <didierdewasseige@gmail.com>
2019-05-23 16:58:08 +0200

    start to answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :cocktails, only: [:index, :show, :new, :create]
 end
+# get 'cocktails/index'
+# get 'cocktails/new'
+# get 'cocktails/create'
+# get 'cocktails/show'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
